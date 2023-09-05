d335614e3d0c2df1691287620ebd49ec3df3b982
leorosignoli <leonard.pereira2@ab-inbev.com>
2023-09-04 23:50:48 -0300

    graphic changes

--- LEOassist/LEOassist/ViewController.swift
+++ LEOassist/LEOassist/ViewController.swift
@@ -15,5 +15,42 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func emailForm(_ sender: UITextField) {
+        sender.borderWidth  = 0.5
+        sender.cornerRadius = 10
+        
+    
+    }
+    
 }
 
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
