32373ad88b6f52e7d7b4b133ba75b01dbb99959f
leorosignoli <leonardo.andrade6@hotmail.com>
2023-11-03 15:14:01 -0300

    improved google login functionality

--- LEOassist/LEOassist/services/GoogleService.swift
+++ LEOassist/LEOassist/services/GoogleService.swift
@@ -1,44 +1,46 @@
-//
-//  GoogleService.swift
-//  LEOassist
-//
-//  Created by Leonardo Andrade Rosignoli on 13/10/23.
-//
-
 import SwiftUI
 import GoogleAPIClientForREST
 import GoogleSignIn
 
-class GoogleCalendarManagerService: NSObject, ObservableObject {
+class GoogleCalendarManagerService: NSObject, ObservableObject, GIDSignInDelegate {
     @Published var events: [GTLRCalendar_Event] = []
     private let calendarService = GTLRCalendarService()
     
-   
     override init() {
-            super.init()
-            
-            // Configure the Google SignIn
-            GIDSignIn.sharedInstance().delegate = self
-            GIDSignIn.sharedInstance().scopes = [kGTLRAuthScopeCalendar]
-            GIDSignIn.sharedInstance().clientID = ApplicationSecrets.GOOGLE_CLIENT_ID
-        }
+        super.init()
         
-        func authenticate() {
-            GIDSignIn.sharedInstance().presentingViewController = UIApplication.shared.windows.first?.rootViewController
-            GIDSignIn.sharedInstance().signIn()
-        }
-        
-    @objc(signIn:didSignInForUser:withError:) func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-            if let error = error {
-                print("Error signing in: \(error.localizedDescription)")
-                return
-            }
-            
-            // Set the authenticated user's credentials for the calendar service
-            calendarService.authorizer = user.authentication.fetcherAuthorizer()
+        // Configure the Google SignIn
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().scopes = [kGTLRAuthScopeCalendar]
+        GIDSignIn.sharedInstance().clientID = ApplicationSecrets.GOOGLE_CLIENT_ID
+    }
+    
+    func authenticate() {
+        GIDSignIn.sharedInstance().presentingViewController = UIApplication.shared.windows.first?.rootViewController
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    // MARK: - GIDSignInDelegate
 
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("Error signing in: \(error.localizedDescription)")
+            return
         }
+        
+        // Set the authenticated user's credentials for the calendar service
+        calendarService.authorizer = user.authentication.fetcherAuthorizer()
+    }
+    
     func fetchEvents() {
+        if GIDSignIn.sharedInstance().hasPreviousSignIn() {
+            fetchEventsInternal()
+        } else {
+            authenticate()
+        }
+    }
+
+    private func fetchEventsInternal() {
         let query = GTLRCalendarQuery_EventsList.query(withCalendarId: "primary")
         query.maxResults = 10
         query.timeMin = GTLRDateTime(date: Date())
@@ -51,7 +53,7 @@ class GoogleCalendarManagerService: NSObject, ObservableObject {
             }
             
             if let eventsResult = result as? GTLRCalendar_Event {
-                print(eventsResult )
+                print(eventsResult)
                 self?.events = [eventsResult]
             }
         }
